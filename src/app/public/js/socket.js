3c0d8a39602240ca6576f3e1cf7544355358e5e1
Jake Bailey <jake.bailey2801@gmail.com>
2021-03-31 20:47:20 +0100

    basic lobby stuff

--- src/app/public/js/socket.js
+++ src/app/public/js/socket.js
@@ -1,51 +1,63 @@
 // const socketConnect = io.connect('ws://localhost/')
-function connect () {
-  const socket = io('ws://localhost/')
+function start () {
+  const socket = io('ws://10.210.70.53/')
 
   const username = document.getElementById('username')
   const submit = document.getElementById('submit')
   console.log(username)
   console.log(submit)
 
-  // socket.emit('startGame', username.value);
-
-  // socket.on('startGame', (user) => {
-
   const pastUid = localStorage.getItem('uid')
   if (pastUid === null) {
+    const submit = document.getElementById('submit')
+    submit.addEventListener('click', () => {
     socket.emit('newPlayer', username.value)
     socket.on('newPlayer', (credentials) => {
       console.log(credentials)
-      const update = document.getElementById('clientID')
-      console.log(update)
-      update.innerText = `ClientID: ${credentials.uid}`
       localStorage.setItem('username', credentials.username)
       localStorage.setItem('uid', credentials.uid)
+      lobbyPage(credentials)
     })
+  })
+
   } else {
+
     const credentials = { username: localStorage.getItem('username'), uid: pastUid }
     socket.emit('pastPlayer', credentials)
     socket.on('pastPlayer', (newCredentials) => {
       console.log(newCredentials)
-      const login = document.getElementById('loginPage')
+      lobbyPage(newCredentials)
+      
+    })
+    
+  }
+  console.log(pastUid)
+}
+
+function lobbyPage(credentials) {
+  const login = document.getElementById('loginPage')
+      
       login.innerHTML = ''
       const lobby = document.createElement('p')
       lobby.className = 'lobby'
       lobby.innerText = 'Please choose a lobby:'
       login.appendChild(lobby)
+
       const clientID = document.createElement('p')
       clientID.id = 'clientID'
       clientID.className = 'client'
-      clientID.innerText = `ClientID: ${newCredentials.uid}\n Username: ${newCredentials.username}`
+      clientID.innerText = `ClientID: ${credentials.uid}\n Username: ${credentials.username}`
       login.appendChild(clientID)
-    })
-  }
-  console.log(pastUid)
-  // })
+
+      const createBtn = document.createElement('button')
+      createBtn.innerText = 'Create'
+      createBtn.className = 'btn'
+      login.appendChild(createBtn)
+    
+      
+      const joinBtn = document.createElement('button')
+      joinBtn.innerText = 'Join'
+      joinBtn.className = 'btn'
+      login.appendChild(joinBtn)
+      
 }
-// async function postData(url = '/user', data = {}) {
-//   const response = await fetch (url, {
-//       method: 'POST',
-//       body: JSON.stringify(credentials)
-//     })
-// }

c4abc41ea0a8dfdb9caa0a500f96843bc472598a
Jake Bailey <jake.bailey2801@gmail.com>
2021-04-02 15:37:43 +0100

    snapping

---
+++ src/app/public/js/dragEvents.js
@@ -0,0 +1,68 @@
+// Drag move listener function - listens to movements when a drag event occurs and keeps the position of the x and y positioning
+function dragMoveListener (event) {
+    const target = event.target
+    // keep the dragged position in the data-x/data-y attributes
+    const x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx
+    const y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy
+  
+    // Translate the element to pixels
+    target.style.webkitTransform =
+    target.style.transform =
+      'translate(' + x + 'px, ' + y + 'px)'
+  
+    // Update the position attributes
+    target.setAttribute('data-x', x)
+    target.setAttribute('data-y', y)
+  }
+  
+  function snapEvent(event) {
+    let dropRect = interact.getElementRect(event.target)
+    dropCenter = {
+      x: dropRect.left + dropRect.width / 2,
+      y: dropRect.top + dropRect.height / 2
+    }
+    console.log(dropCenter)
+    console.log(event)
+    event.draggable.draggable({
+      snap: {
+        targets: [dropCenter]
+      }
+    });
+  }
+
+function dragEnter(event) {
+    const draggableElement = event.relatedTarget
+      const dropzoneElement = event.target
+  
+      if (dropzoneElement.classList.contains('occupied') == false) {
+        // Changes styling of div on posibility of a drop occuring (feedback from possible drop)
+        dropzoneElement.classList.add('drop-target')
+        draggableElement.classList.add('can-drop')
+        snapEvent(event)
+      }
+  }
+  
+  function dragLeave(event) {
+    // When target leaves dropzone, the possible drop feedback styling is removed
+    event.target.classList.remove('drop-target')
+    event.relatedTarget.classList.remove('can-drop')
+    if (event.relatedTarget.classList.contains('dropped-tile')) {
+      event.target.classList.remove('occupied')
+      event.relatedTarget.classList.remove('dropped-tile')
+    }
+  }
+  
+  function onDrop(event) {
+    // When target is dropped, the possible drop feedback styling is removed
+    event.target.classList.remove('drop-target')
+    event.relatedTarget.classList.remove('can-drop')
+    event.target.classList.add('occupied')
+    event.relatedTarget.classList.add('dropped-tile')
+    findPlacement(event.target.classList[0])
+  }
+  
+  function dropDeactivate(event) {
+    // // When target is no longer being dragged, the drop active feedback styling is removed
+    event.target.classList.remove('drop-active')
+    event.target.classList.remove('drop-target')
+  }
--- src/app/public/js/draggable.js
+++ src/app/public/js/draggable.js
@@ -1,3 +1,5 @@
+
+
 // Drag move listener function - listens to movements when a drag event occurs and keeps the position of the x and y positioning
 function dragMoveListener (event) {
   const target = event.target
@@ -15,6 +17,102 @@ function dragMoveListener (event) {
   target.setAttribute('data-y', y)
 }
 
+function snapEvent(event) {
+  let dropRect = interact.getElementRect(event.target)
+  dropCenter = {
+    x: dropRect.left + dropRect.width / 2,
+    y: dropRect.top + dropRect.height / 2
+  }
+  console.log(dropCenter)
+  console.log(event)
+  event.draggable.draggable({
+    snap: {
+      targets: [dropCenter]
+    }
+  });
+}
+
+var startPos = null
+interact('.drag-drop').draggable({
+  snap: {
+    targets: [startPos],
+    range: Infinity,
+    relativePoints: [ { x: 0.5, y: 0.5 } ],
+    endOnly: true
+  },
+  onstart: function (event) {
+      var rect = interact.getElementRect(event.target);
+
+      // record center point when starting the very first a drag
+      startPos = {
+        x: rect.left + rect.width  / 2,
+        y: rect.top  + rect.height / 2
+      }
+
+    event.interactable.draggable({
+      snap: {
+        targets: [startPos]
+      }
+    });
+  },
+  // call this function on every dragmove event
+  onmove: function (event) {
+        // keep the dragged position in the data-x/data-y attributes
+        x = (parseFloat(event.target.getAttribute('data-x')) || 0) + event.dx,
+        y = (parseFloat(event.target.getAttribute('data-y')) || 0) + event.dy;
+
+    // translate the element
+    event.target.style.webkitTransform =
+    event.target.style.transform =
+      'translate(' + x + 'px, ' + y + 'px)';
+
+    // update the posiion attributes
+    event.target.setAttribute('data-x', x);
+    event.target.setAttribute('data-y', y);
+    event.target.classList.add('getting--dragged');
+  },
+  onend: function (event) {
+    event.target.classList.remove('getting--dragged')
+  }
+})
+
+function dragEnter(event) {
+  const draggableElement = event.relatedTarget
+    const dropzoneElement = event.target
+
+    if (dropzoneElement.classList.contains('occupied') == false) {
+      // Changes styling of div on posibility of a drop occuring (feedback from possible drop)
+      dropzoneElement.classList.add('drop-target')
+      draggableElement.classList.add('can-drop')
+      snapEvent(event)
+    }
+}
+
+function dragLeave(event) {
+  // When target leaves dropzone, the possible drop feedback styling is removed
+  event.target.classList.remove('drop-target')
+  event.relatedTarget.classList.remove('can-drop')
+  if (event.relatedTarget.classList.contains('dropped-tile')) {
+    event.target.classList.remove('occupied')
+    event.relatedTarget.classList.remove('dropped-tile')
+  }
+}
+
+function onDrop(event) {
+  // When target is dropped, the possible drop feedback styling is removed
+  event.target.classList.remove('drop-target')
+  event.relatedTarget.classList.remove('can-drop')
+  event.target.classList.add('occupied')
+  event.relatedTarget.classList.add('dropped-tile')
+  findPlacement(event.target.classList[0])
+}
+
+function dropDeactivate(event) {
+  // // When target is no longer being dragged, the drop active feedback styling is removed
+  event.target.classList.remove('drop-active')
+  event.target.classList.remove('drop-target')
+}
+
 interact('.box').dropzone({
   // Only accepts elements with id 'droppable' to be dropped into the dropzone
   accept: '.drag-drop',
@@ -23,58 +121,19 @@ interact('.box').dropzone({
 
   // Listens for drop events e.g. drag enters a dropzone, drag leaves a dropzone
   ondragenter: function (event) {
-    const draggableElement = event.relatedTarget
-    const dropzoneElement = event.target
-
-    if (dropzoneElement.classList.contains('occupied') == false) {
-      // Changes styling of div on posibility of a drop occuring (feedback from possible drop)
-      dropzoneElement.classList.add('drop-target')
-      draggableElement.classList.add('can-drop')
-    }
-
-    dropRect = interact.getElementRect(dropzoneElement),
-    dropCenter = {
-      x: dropRect.left + dropRect.width / 2,
-      y: dropRect.top + dropRect.height / 2
-    }
-
-    event.draggable.draggable({
-      modifiers: [
-        // interact.modifiers.snap({
-        //   targets: [
-        //     dropCenter
-        //   ],
-        //   range: Infinity,
-        //   relativePoints: [{ x: 0, y: 0 }]
-        // })
-      ]
-
-    })
+    dragEnter(event)
   },
 
   ondragleave: function (event) {
-    // When target leaves dropzone, the possible drop feedback styling is removed
-    event.target.classList.remove('drop-target')
-    event.relatedTarget.classList.remove('can-drop')
-    if (event.relatedTarget.classList.contains('dropped-tile')) {
-      event.target.classList.remove('occupied')
-      event.relatedTarget.classList.remove('dropped-tile')
-    }
+    dragLeave(event)
   },
 
   ondrop: function (event) {
-    // When target is dropped, the possible drop feedback styling is removed
-    event.target.classList.remove('drop-target')
-    event.relatedTarget.classList.remove('can-drop')
-    event.target.classList.add('occupied')
-    event.relatedTarget.classList.add('dropped-tile')
-    findPlacement(event.target.classList[0])
+    onDrop(event)
   },
 
   ondropdeactivate: function (event) {
-    // // When target is no longer being dragged, the drop active feedback styling is removed
-    event.target.classList.remove('drop-active')
-    event.target.classList.remove('drop-target')
+    dropDeactivate(event)
   }
 })
 
@@ -82,43 +141,23 @@ interact('.box-light-pink').dropzone({
   // Only accepts elements with id 'droppable' to be dropped into the dropzone
   accept: '.drag-drop',
   // Require a 75% element overlap for a drop to be possible
-  overlap: 1,
+  overlap: 0.5,
 
   // Listens for drop events e.g. drag enters a dropzone, drag leaves a dropzone
   ondragenter: function (event) {
-    const draggableElement = event.relatedTarget
-    const dropzoneElement = event.target
-
-    if (dropzoneElement.classList.contains('occupied') == false) {
-      // Changes styling of div on posibility of a drop occuring (feedback from possible drop)
-      dropzoneElement.classList.add('drop-target')
-      draggableElement.classList.add('can-drop')
-    }
+    dragEnter(event)
   },
 
   ondragleave: function (event) {
-    // When target leaves dropzone, the possible drop feedback styling is removed
-    event.target.classList.remove('drop-target')
-    event.relatedTarget.classList.remove('can-drop')
-    if (event.relatedTarget.classList.contains('dropped-tile')) {
-      event.target.classList.remove('occupied')
-      event.relatedTarget.classList.remove('dropped-tile')
-    }
+    dragLeave(event)
   },
 
   ondrop: function (event) {
-    // When target is dropped, the possible drop feedback styling is removed
-    event.target.classList.remove('drop-target')
-    event.relatedTarget.classList.remove('can-drop')
-    event.target.classList.add('occupied')
-    event.relatedTarget.classList.add('dropped-tile')
-    findPlacement(event.target.classList[0])
+    onDrop(event)
   },
 
   ondropdeactivate: function (event) {
-    // // When target is no longer being dragged, the drop active feedback styling is removed
-    event.target.classList.remove('drop-active')
-    event.target.classList.remove('drop-target')
+    dropDeactivate(event)
   }
 })
 
@@ -126,43 +165,23 @@ interact('.box-light-blue').dropzone({
   // Only accepts elements with id 'droppable' to be dropped into the dropzone
   accept: '.drag-drop',
   // Require a 75% element overlap for a drop to be possible
-  overlap: 1,
+  overlap: 0.5,
 
   // Listens for drop events e.g. drag enters a dropzone, drag leaves a dropzone
   ondragenter: function (event) {
-    const draggableElement = event.relatedTarget
-    const dropzoneElement = event.target
-
-    if (dropzoneElement.classList.contains('occupied') == false) {
-      // Changes styling of div on posibility of a drop occuring (feedback from possible drop)
-      dropzoneElement.classList.add('drop-target')
-      draggableElement.classList.add('can-drop')
-    }
+    dragEnter(event)
   },
 
   ondragleave: function (event) {
-    // When target leaves dropzone, the possible drop feedback styling is removed
-    event.target.classList.remove('drop-target')
-    event.relatedTarget.classList.remove('can-drop')
-    if (event.relatedTarget.classList.contains('dropped-tile')) {
-      event.target.classList.remove('occupied')
-      event.relatedTarget.classList.remove('dropped-tile')
-    }
+    dragLeave(event)
   },
 
   ondrop: function (event) {
-    // When target is dropped, the possible drop feedback styling is removed
-    event.target.classList.remove('drop-target')
-    event.relatedTarget.classList.remove('can-drop')
-    event.target.classList.add('occupied')
-    event.relatedTarget.classList.add('dropped-tile')
-    findPlacement(event.target.classList[0])
+    onDrop(event)
   },
 
   ondropdeactivate: function (event) {
-    // // When target is no longer being dragged, the drop active feedback styling is removed
-    event.target.classList.remove('drop-active')
-    event.target.classList.remove('drop-target')
+    dropDeactivate(event)
   }
 })
 
@@ -170,43 +189,23 @@ interact('.box-dark-pink').dropzone({
   // Only accepts elements with id 'droppable' to be dropped into the dropzone
   accept: '.drag-drop',
   // Require a 75% element overlap for a drop to be possible
-  overlap: 1,
+  overlap: 0.5,
 
   // Listens for drop events e.g. drag enters a dropzone, drag leaves a dropzone
   ondragenter: function (event) {
-    const draggableElement = event.relatedTarget
-    const dropzoneElement = event.target
-
-    if (dropzoneElement.classList.contains('occupied') == false) {
-      // Changes styling of div on posibility of a drop occuring (feedback from possible drop)
-      dropzoneElement.classList.add('drop-target')
-      draggableElement.classList.add('can-drop')
-    }
+    dragEnter(event)
   },
 
   ondragleave: function (event) {
-    // When target leaves dropzone, the possible drop feedback styling is removed
-    event.target.classList.remove('drop-target')
-    event.relatedTarget.classList.remove('can-drop')
-    if (event.relatedTarget.classList.contains('dropped-tile')) {
-      event.target.classList.remove('occupied')
-      event.relatedTarget.classList.remove('dropped-tile')
-    }
+    dragLeave(event)
   },
 
   ondrop: function (event) {
-    // When target is dropped, the possible drop feedback styling is removed
-    event.target.classList.remove('drop-target')
-    event.relatedTarget.classList.remove('can-drop')
-    event.target.classList.add('occupied')
-    event.relatedTarget.classList.add('dropped-tile')
-    findPlacement(event.target.classList[0])
+    onDrop(event)
   },
 
   ondropdeactivate: function (event) {
-    // // When target is no longer being dragged, the drop active feedback styling is removed
-    event.target.classList.remove('drop-active')
-    event.target.classList.remove('drop-target')
+    dropDeactivate(event)
   }
 })
 
@@ -214,63 +213,22 @@ interact('.box-dark-blue').dropzone({
   // Only accepts elements with id 'droppable' to be dropped into the dropzone
   accept: '.drag-drop',
   // Require a 75% element overlap for a drop to be possible
-  overlap: 1,
+  overlap: 0.5,
 
   // Listens for drop events e.g. drag enters a dropzone, drag leaves a dropzone
   ondragenter: function (event) {
-    const draggableElement = event.relatedTarget
-    const dropzoneElement = event.target
-
-    if (dropzoneElement.classList.contains('occupied') == false) {
-      // Changes styling of div on posibility of a drop occuring (feedback from possible drop)
-      dropzoneElement.classList.add('drop-target')
-      draggableElement.classList.add('can-drop')
-    }
+    dragEnter(event)
   },
 
   ondragleave: function (event) {
-    // When target leaves dropzone, the possible drop feedback styling is removed
-    event.target.classList.remove('drop-target')
-    event.relatedTarget.classList.remove('can-drop')
-    if (event.relatedTarget.classList.contains('dropped-tile')) {
-      event.target.classList.remove('occupied')
-      event.relatedTarget.classList.remove('dropped-tile')
-    }
+    dragLeave(event)
   },
 
   ondrop: function (event) {
-    // When target is dropped, the possible drop feedback styling is removed
-    event.target.classList.remove('drop-target')
-    event.relatedTarget.classList.remove('can-drop')
-    event.target.classList.add('occupied')
-    event.relatedTarget.classList.add('dropped-tile')
-    findPlacement(event.target.classList[0])
+    onDrop(event)
   },
 
   ondropdeactivate: function (event) {
-    // // When target is no longer being dragged, the drop active feedback styling is removed
-    event.target.classList.remove('drop-active')
-    event.target.classList.remove('drop-target')
+    dropDeactivate(event)
   }
-})
-const startPos = null
-interact('.drag-drop').draggable({
-
-  inertia: true,
-  modifiers: [
-    interact.modifiers.restrictRect({
-      restriction: '.parent',
-      endOnly: true
-    }),
-    // interact.modifiers.snap({
-    //   // targets: [
-    //   //   interact.snappers.grid({ x: 30, y: 30 })
-    //   // ],
-    //   range: Infinity,
-    //   relativePoints: [{ x: 0, y: 0 }]
-    // })
-  ],
-  autoScroll: true,
-  listeners: { move: dragMoveListener }
-
 })
